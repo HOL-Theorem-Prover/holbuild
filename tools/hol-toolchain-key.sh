1394e942a16b99351b581d9a83100cc3d3e56251
Ramana Kumar <ramana@member.fsf.org>
2026-06-10 15:30:16 +0100

    Use exact sml code for hol-toolchain-key

--- tools/hol-toolchain-key.sh
+++ tools/hol-toolchain-key.sh
@@ -6,16 +6,6 @@ if [[ $# -ne 1 ]]; then
   exit 2
 fi
 
-rev=$1
-git_url=${HOLBUILD_CANONICAL_HOL_GIT:-https://github.com/HOL-Theorem-Prover/HOL.git}
+script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 poly=${HOLBUILD_POLY:-poly}
-poly_version=$("$poly" -v | awk '{$1=$1; print}')
-
-{
-  printf 'holbuild-hol-toolchain-v1\n'
-  printf 'git=%s\n' "$git_url"
-  printf 'rev=%s\n' "$rev"
-  printf 'poly=%s\n' "$poly"
-  printf 'poly_version=%s\n' "$poly_version"
-  printf 'build_args=\n'
-} | sha1sum | awk '{print $1}'
+HOLBUILD_TOOLCHAIN_KEY_REV=$1 exec "$poly" --script "$script_dir/hol-toolchain-key.sml"
